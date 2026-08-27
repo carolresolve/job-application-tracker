95967b544f81487dbd52e59e41d8e8ad365fbc44
Ana Carolina <70481902+carolresolve@users.noreply.github.com>
2026-08-27 10:51:48 +0100

    feat: adiciona layout base HTML/CSS sem framework e dashboard inicial com contagens do MongoDB

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+from database.conexao import conectar_banco
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    banco = conectar_banco()
+    
+    # Contagem de registros em cada coleção
+    total_cand = banco["candidatos"].count_documents({})
+    total_emp = banco["empresas"].count_documents({})
+    total_cand_vagas = banco["candidaturas"].count_documents({})
+    
+    return render_template(
+        "index.html", 
+        total_candidatos=total_cand,
+        total_empresas=total_emp,
+        total_candidaturas=total_cand_vagas
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
